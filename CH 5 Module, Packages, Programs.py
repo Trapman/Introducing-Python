1b0b2f4630bdd9fe910156bf1d33eed5b430298a
Trapman <david.a.shuttleworth@gmail.com>
2020-02-21 22:50:10 -0500

    Update CH 5 Module, Packages, Programs.py

--- CH 5 Module, Packages, Programs.py
+++ CH 5 Module, Packages, Programs.py
@@ -61,6 +61,66 @@ def forecast():
 get() #when accessing a dictionary with a missing/nonexistant key it raises an error, get() returns a default value to avoid the exception
 setdefault() # is like get() but ALSO assigns an item to the dict if the key is missing
 
+#ex
+periodic_table = {'hydrogen': 1, 'helium': 2}
+
+#if the key was NOT already in the dict, the new value is used
+carbon = periodic_table.setdefault('carbon', 12)
+
+#now when you call the dict, it's added
+print(perdioc_table)
+# returns {'hydrogen': 1, 'carbon' : 12, 'helium': 2}
+# note that this DOES NOT WORK if we try to assign a value to already existing key
+
+defaultdict() # similar, but specifies the default value for any new key up front
+# ex: here we will we the function int() and return the integer 0
+from collections import defaultdict
+periodic_table = defaultdict(int) # now any missing value will be an int with the value 0
+
+periodic_table['hydrogen'] = 1
+periodic_table['lead']           # if we don't assign a value here, it automatically assigns a value, 0 here
+
+# you can use the following functions with defeaultdict():
+int() # returns 0
+list() # returns an empty list, ([])
+dict() # returns and empty dict, ({})
+
+# COUNTER() ###################################
+# you can use - , + , & , |
+from collections import Counter
+breakfast = ['spam', 'eggs', 'eggs', 'spam', 'eggs']
+breakfast_counter = Counter(breakfast)
+breakfast_counter # returns Counter({'spam':2, 'eggs':3})
+
+# you can combine counters too
+lunch = ['burger', 'hoagie', 'hoagie', 'hoagie', 'burger']
+lunch_counter = Counter(lunch)
+
+breakfast_counter + lunch_counter
+# returns Counter({'spam':2, 'eggs':3, 'burger': 2, 'hoagie': 3})
+
+# you can also check what you had for lunch that you didn't have for breakfast
+breakfast_counter - lunch_counter
+
+# you can find common items by using the intersection operator with &
+# you can get all items using the union operator with |
+
+# ORDER BY KEY WITH OrderedDict() ###########################################################################
+# this remembers the order of key addition and returns them in the same order
+from collections import OrderedDict
+quotes = OrderedDict([
+  ('Trap', 'Yo buls bet the ML'),
+  ('Brad', 'Yarko how much are you betting?'),
+  ('Yark', 'ML or spread???')
+])
+
+for bul in quotes:
+  print(bul)
+       
+
+
+
+
 
 
 
